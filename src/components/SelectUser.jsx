1b48c1cbe74cc3fa5bd22fc08e1a79da5062a8d4
bomlang <bomlang4211@gmail.com>
2023-08-28 02:22:00 +0900

    [modify] selectUser의 데이터를 users로 변경하였습니다

--- src/components/SelectUser.jsx
+++ src/components/SelectUser.jsx
@@ -1,44 +1,28 @@
 import GoToEnvelope from "./GoToEnvelope";
 import { useUserList } from "@/hooks/useUserList";
-import PocketBase from "pocketbase";
 
 function SelectUser({ searchTerm }) {
   const { data } = useUserList();
 
-  // const filterUsers = data.filter((user) =>
-  //   user.name.toLowerCase().includes(searchTerm.toLowerCase()),
-
-  // );
-  console.log(data);
-  const handleCreateMessageToUser = async () => {
-    const mailData = {
-      fromId: "test",
-      toId: "test",
-      gifStamp: "https://example.com",
-      content: "test",
-      envelope: "white",
-    };
-
-    const pb = new PocketBase("https://likelion-mailbox.pockethost.io");
-    const record = await pb.collection("test_message").create(mailData);
-    console.log(record);
-  };
+  const filterUsers = data.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   return (
-    <ul className="grid grid-cols-3 m-10 gap-y-5 justify-items-center items-center">
-      {filterUsers.map((item) => {
-        const keyId = item.id;
-        const name = item.user_name;
-        return (
-          <li
-            onClick={handleCreateMessageToUser}
-            key={keyId}
-            className="w-[300px] h-[64px] rounded-[10px] bg-inputYellow hover:bg-lionYellow hover:cursor-pointer"
-          >
-            <GoToEnvelope name={name} keyId={keyId} />
-          </li>
-        );
-      })}
+    <ul className="grid grid-cols-3 m-10 gap-y-5 justify-items-center items-center text-3xl">
+      {filterUsers &&
+        filterUsers?.map((item) => {
+          const keyId = item.id;
+          const name = item.name;
+          return (
+            <li
+              key={keyId}
+              className="w-[300px] h-[64px] rounded-[10px] bg-inputYellow hover:bg-lionYellow hover:cursor-pointer"
+            >
+              <GoToEnvelope name={name} keyId={keyId} />
+            </li>
+          );
+        })}
     </ul>
   );
 }
